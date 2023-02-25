67ec41d10a42da47e15cb7d2e9d286dcb5775727
Kirill <zabavin@gmail.com>
2023-02-25 18:44:06 +0300

    extract setting variables ftom settings.py to .env

--- OikosShop/OikosShop/settings.py
+++ OikosShop/OikosShop/settings.py
@@ -21,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f$cyft7qx!8t#&wo7=161e%&zcfufpit_kf5x(wrbrs&-@^53^'
+#SECRET_KEY = 'django-insecure-f$cyft7qx!8t#&wo7=161e%&zcfufpit_kf5x(wrbrs&-@^53^'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
 # Application definition
 
